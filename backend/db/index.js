3a47b1526c5a78ca54947214d0d178e98de62b3a
jaisriv788 <srivastavajai845@gmail.com>
2025-06-20 13:02:55 +0530

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/storage",
+      process.env.MONGODB_URL || "mongodb://localhost:27017/storage",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
